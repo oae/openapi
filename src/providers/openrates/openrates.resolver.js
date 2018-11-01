ded1a1c39855a4159c18cb8922b342bc27dbf5db
halitogunc <hltognc@gmail.com>
2018-11-02 00:17:50 +0300

    Add historical rates support #12

--- src/providers/openrates/openrates.resolver.js
+++ src/providers/openrates/openrates.resolver.js
@@ -1,8 +1,17 @@
+const moment = require('moment');
 const { resolveAlias } = require('../../utils');
 
 const client = require('./client');
 
-const createLatestRatesResolver = () => async (obj, { from, to } = {}, context, info) => {
+const resolveDate = date => moment(date).format('YYYY-MM-DD');
+
+const createLatestRatesResolver = () => async (obj, { from, to, date } = {}, context, info) => {
+  if (date) {
+    const resolvedDate = resolveDate(date);
+    const res = await client.get(resolvedDate, { params: { base: from } });
+    return { ...res.data, to };
+  }
+
   const res = await client.get('latest', {
     params: {
       base: from,
@@ -11,7 +20,7 @@ const createLatestRatesResolver = () => async (obj, { from, to } = {}, context, 
   return { ...res.data, to };
 };
 
-const resolveRates = () => (obj, { temp } = {}, context, info) => {
+const resolveRates = () => (obj, _, context, info) => {
   const { to, rates } = obj;
   return to.reduce((acc, val) => {
     acc.push({ to: val, rate: rates[val] });
