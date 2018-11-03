2f2c80c8d444c741e97213384440e86ed7cd8603
Alperen Elhan <alperen@elhan.org>
2018-11-03 23:38:49 +0300

    Return enabled providers query result

--- packages/common/common.resolver.js
+++ packages/common/common.resolver.js
@@ -1,7 +1,17 @@
-// const providers = require('../');
+const fp = require('lodash/fp');
+
+const { openApi } = require('@openapi/core');
 
 module.exports = {
   Query: {
-    providers: (obj, args, context, info) => [{ name: 'hop' }],
+    providers: (obj, args, context, info) => {
+      const { enabledProviders } = openApi.getConfig();
+      return fp.flow(
+        fp.map(fp.castArray),
+        fp.map(([provider, options = {}]) => ({
+          name: provider,
+        }))
+      )(enabledProviders);
+    },
   },
 };
