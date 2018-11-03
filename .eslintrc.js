eeafb9a7e576df0493c6d0a7415575ef34e41bfc
Yasin Uslu <nepjua@gmail.com>
2018-11-03 20:28:49 +0300

    Add jest-extended for complex assertions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: ['jest', 'prettier'],
-  extends: ['airbnb-base', 'plugin:jest/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'airbnb-base',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
   parser: 'babel-eslint',
   env: {
     node: true,
@@ -9,6 +13,13 @@ module.exports = {
     'global-require': 'off',
     'import/no-dynamic-require': 'off',
     'no-unused-vars': ['error', { args: 'none' }],
-    'prettier/prettier': ['error', { trailingComma: 'es5', singleQuote: true, printWidth: 100 }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/*.test.js', '**/*.spec.js', 'config/**/*.js'] },
+    ],
+    'prettier/prettier': [
+      'error',
+      { trailingComma: 'es5', singleQuote: true, printWidth: 100 },
+    ],
   },
 };
