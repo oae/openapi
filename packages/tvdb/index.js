3fe88ac9651e4259916d27049adff3f9ef8bfaa2
Alperen Elhan <alperen@elhan.org>
2018-11-09 01:01:43 +0300

    Update test for tvdb

--- packages/tvdb/index.js
+++ packages/tvdb/index.js
@@ -1,8 +1,10 @@
 const { loadTypeDefs, loadResolvers } = require('@openapi/core/utils');
 const context = require('./context');
 const queue = require('./queue');
+const { login } = require('./auth');
 
 async function init() {
+  await login();
   await queue.init();
 
   return {
