754447cda2fb93e84790f1e97807c6f98bc9b568
halitogunc <hltognc@gmail.com>
2018-11-11 23:56:41 +0300

    Add anime category query

---
+++ packages/kitsu/constants.js
@@ -0,0 +1,8 @@
+const KeyType = {
+  CATEGORY: 'categories',
+};
+
+module.exports = {
+  KeyType,
+  DEFAULT_LIMIT: 30,
+};
--- packages/kitsu/kitsu.resolver.js
+++ packages/kitsu/kitsu.resolver.js
@@ -1,17 +1,48 @@
 const { resolveAlias } = require('@openapi/core/utils');
+const fp = require('lodash/fp');
 const client = require('./client');
+const { DEFAULT_LIMIT } = require('./constants');
 
-const createCategoriesResolver = () => async (obj, _, context, info) => {
-  const res = await client.get('categories');
-  return { ...res.data };
+const createCategoriesResolver = () => async (
+  obj,
+  { limit = DEFAULT_LIMIT } = {},
+  context,
+  info
+) => {
+  const res = await client.get('categories', {
+    params: {
+      'page[limit]': limit,
+    },
+  });
+  return res.data.data;
+};
+
+const getCovers = key => obj => {
+  const images = fp.get(key, obj);
+  if (images) {
+    return {
+      tiny: images.tiny,
+      small: images.small,
+      medium: images.medium,
+      large: images.large,
+      original: images.original,
+    };
+  }
+  return null;
 };
 
 module.exports = {
   Query: {
-    categories: createCategoriesResolver(),
+    animeCategories: createCategoriesResolver(),
   },
 
   Category: {
-    id: resolveAlias('id'),
+    title: resolveAlias('attributes.title'),
+    description: resolveAlias('attributes.description'),
+    slug: resolveAlias('attributes.slug'),
+    nsfw: resolveAlias('attributes.nsfw'),
+    createdAt: resolveAlias('attributes.createdAt'),
+    updatedAt: resolveAlias('attributes.updatedAt'),
+    covers: getCovers('attributes.image'),
   },
 };
