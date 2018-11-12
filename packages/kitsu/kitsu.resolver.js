0b951f7b43778048e0ddde061d68a12752e13b4e
halitogunc <hltognc@gmail.com>
2018-11-12 23:53:50 +0300

    Add anime query without category

--- packages/kitsu/kitsu.resolver.js
+++ packages/kitsu/kitsu.resolver.js
@@ -1,7 +1,17 @@
 const { resolveAlias } = require('@openapi/core/utils');
+const format = require('date-fns/format');
 const fp = require('lodash/fp');
 const client = require('./client');
 const { DEFAULT_LIMIT } = require('./constants');
+
+const createAnimesResolver = () => async (obj, { limit = DEFAULT_LIMIT } = {}, context, info) => {
+  const res = await client.get('anime', {
+    params: {
+      'page[limit]': 5,
+    },
+  });
+  return res.data.data;
+};
 
 const createCategoriesResolver = () => async (
   obj,
@@ -17,7 +27,7 @@ const createCategoriesResolver = () => async (
   return res.data.data;
 };
 
-const getCovers = key => obj => {
+const getAnimeImage = key => obj => {
   const images = fp.get(key, obj);
   if (images) {
     return {
@@ -31,18 +41,55 @@ const getCovers = key => obj => {
   return null;
 };
 
+const resolveAnimeTitle = key => obj => {
+  const title = fp.get(key, obj);
+  if (title) {
+    return {
+      en: title.en,
+      enJp: title.en_jp,
+      jaJp: title.ja_jp,
+      canonicalTitle: obj.attributes.canonicalTitle,
+    };
+  }
+  return null;
+};
+
+const resolveDate = key => obj => {
+  const date = fp.get(key, obj);
+  return format(date);
+};
+
+const resolveTitle = () => obj => obj.attributes.title;
+
 module.exports = {
   Query: {
+    animes: createAnimesResolver(),
     animeCategories: createCategoriesResolver(),
   },
 
+  Anime: {
+    title: resolveAnimeTitle('attributes.titles'),
+    slug: resolveAlias('attributes.slug'),
+    synopsis: resolveAlias('attributes.synopsis'),
+    createdAt: resolveAlias('attributes.createdAt'),
+    updatedAt: resolveAlias('attributes.updatedAt'),
+    status: resolveAlias('attributes.status'),
+    averageRating: resolveAlias('attributes.averageRating'),
+    startDate: resolveDate('attributes.startDate'),
+    endDate: resolveDate('attributes.endDate'),
+    posterImage: getAnimeImage('attributes.posterImage'),
+    coverImage: getAnimeImage('attributes.coverImage'),
+    episodeCount: resolveAlias('attributes.episodeCount'),
+    nsfw: resolveAlias('attributes.nsfw'),
+  },
+
   Category: {
-    title: resolveAlias('attributes.title'),
+    title: resolveTitle(),
     description: resolveAlias('attributes.description'),
     slug: resolveAlias('attributes.slug'),
     nsfw: resolveAlias('attributes.nsfw'),
     createdAt: resolveAlias('attributes.createdAt'),
     updatedAt: resolveAlias('attributes.updatedAt'),
-    covers: getCovers('attributes.image'),
+    cover: getAnimeImage('attributes.image'),
   },
 };
